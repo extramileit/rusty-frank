5d45201b45124abea559033a8df19d729c16d171
Kurt Hartmann <extramileit@gmail.com>
2024-09-15 11:26:48 -0500

    receive input from command line

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # rusty-frank
 
 Let's build something in rust!
+
+## resources
+
+[rust tutorial](https://fitech101.aalto.fi/programming-languages/rust/8-interaction-input-and-os/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("hello world");
+    let mut input: String = Default::default();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("{} bytes read", n);
+            println!("{}", input);
+        }
+        Err(error) => println!("error: {error}")
+    }
 }
